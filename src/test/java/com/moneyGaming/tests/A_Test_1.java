b1df80c580857688c6845873e7b65d61e5521ad5
imran1066 <85904544+imran1066@users.noreply.github.com>
2021-09-27 04:36:09 +0100

    I have made some minor changes.

--- src/test/java/com/moneyGaming/tests/A_Test_1.java
+++ src/test/java/com/moneyGaming/tests/A_Test_1.java
@@ -1,4 +1,43 @@
 package com.moneyGaming.tests;
 
+import com.Utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class A_Test_1 {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() throws InterruptedException {
+//opens me a chrome driver
+        WebDriver driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.get("https://moneygaming.qa.gameaccount.com/sign-up.shtml");
+        Thread.sleep(300000);
+    }
+
+    @Test
+    public void test1() throws InterruptedException {
+        WebElement nameBox= driver.findElement(By.cssSelector("#forename"));
+
+
+        nameBox.sendKeys("Adam");
+
+        WebElement surnameBox = driver.findElement(By.xpath("//input[@name='map(lastName)']"));
+
+        surnameBox.sendKeys("Smith");
+        Thread.sleep(3000);
+
+    }
+    @AfterMethod
+    public void tearDown() {
+        driver.close();
+
+    }
+
+
 }
