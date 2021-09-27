330a50029f29451a54a6d2e9d3b4b5108ec0b73f
imran1066 <85904544+imran1066@users.noreply.github.com>
2021-09-27 12:14:03 +0100

    ImranBranch commit 270921 1212

--- src/test/java/com/moneyGaming/tests/ImranTest_270921_1142.java
+++ src/test/java/com/moneyGaming/tests/ImranTest_270921_1142.java
@@ -5,6 +5,10 @@ import org.testng.annotations.Test;
 public class ImranTest_270921_1142 {
     @Test
     public void testImran(){
+
         System.out.println("My beautiful test on 270921 1144");
+        System.out.println("My another beatiful commit on 270921 1212");
     }
+
+
 }
